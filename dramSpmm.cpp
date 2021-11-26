5cded2cf9d548952222227184f9ac14924f23c5e
fishming <1661342068@qq.com>
2021-11-26 18:59:20 +0800

    fix bugs

--- dramSpmm.cpp
+++ dramSpmm.cpp
@@ -1,62 +1,54 @@
 // #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <vector>
-#include "spmm.h"
 #include "./util/mmio.hpp"
 #include "./util/util.hpp"
+#include "spmm.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 
 // dense [nrow, kdim]
 // out [nrow, kdim]
 
-template<class T>
-class DramArray
-{
+template <class T> class DramArray {
 public:
-    DramArray(int num)
-    {
-        size = num * sizeof(T);
-        array = (T*) malloc(size);
-    }
-    ~DramArray()
-    {
-        free(array);
-    }
-    size_t size;
-    T* array;
+  DramArray(int num) {
+    size = num * sizeof(T);
+    array = (T *)malloc(size);
+  }
+  ~DramArray() { free(array); }
+  size_t size;
+  T *array;
 };
 
-int main(int argc, char** argv)
-{
-    int nrow;
-    int ncol;
-    int nnz;
-    int kdim = 4;
-    std::vector<int> row_CSR, row_COO, col_COO;
-    std::vector<float> values_COO;
-    readMtx<float>(argv[1], row_COO, col_COO, values_COO, nrow, ncol, nnz);
-    COO_to_CSR(row_CSR, row_COO, nnz, nrow);
+int main(int argc, char **argv) {
+  int nrow;
+  int ncol;
+  int nnz;
+  int kdim = 4;
+  std::vector<int> row_CSR, row_COO, col_COO;
+  std::vector<float> values_COO;
+  readMtx<float>(argv[1], row_COO, col_COO, values_COO, nrow, ncol, nnz);
+  COO_to_CSR(row_CSR, row_COO, nnz, nrow);
 
-    DramArray<int> csrptr(nrow);
-    DramArray<int> colind(nnz);
-    DramArray<float> values(nnz);
-    DramArray<float> dense(nrow * kdim);
-    DramArray<float> out(nrow * kdim);
+  DramArray<int> csrptr(nrow + 1);
+  DramArray<int> colind(nnz);
+  DramArray<float> values(nnz);
+  DramArray<float> dense(nrow * kdim);
+  DramArray<float> out(nrow * kdim);
 
-    std::copy(row_CSR.begin(), row_CSR.end(), csrptr.array);
-    std::copy(col_COO.begin(), col_COO.end(), colind.array);
-    std::copy(values_COO.begin(), values_COO.end(), values.array);
-    
-    srand(0);
-    for(int i = 0;i< nrow * kdim;i++)
-    {
-        out.array[i] = ((rand() % INT_MAX) / INT_MAX * 1.0);
-        dense.array[i] = ((rand() % INT_MAX) / INT_MAX * 1.0);
-    }
-    unsigned long start = now();
-    spmm(nrow, kdim, csrptr.array, colind.array, values.array, dense.array, out.array);
-    unsigned long end = now();
-    printf("finished %ld\n us", end - start);
-    return 0;
+  std::copy(row_CSR.begin(), row_CSR.end(), csrptr.array);
+  std::copy(col_COO.begin(), col_COO.end(), colind.array);
+  std::copy(values_COO.begin(), values_COO.end(), values.array);
+
+  srand(0);
+  for (int i = 0; i < nrow * kdim; i++) {
+    dense.array[i] = ((rand() % INT_MAX) / INT_MAX * 1.0);
+  }
+  unsigned long start = now();
+  spmm(nrow, kdim, csrptr.array, colind.array, values.array, dense.array,
+       out.array);
+  unsigned long end = now();
+  printf("finished %ld\n us", end - start);
+  return 0;
 }
--- spmm.h
+++ spmm.h
@@ -6,50 +6,40 @@
 #include <sys/time.h>
 #include <vector>
 
-unsigned long now()
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000000 + tv.tv_usec;
+unsigned long now() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-void spmm(int nrow, int kdim, int *csrptr, int *colind, float *values, float* dense, float* out)
-{
-    for(int i = 0;i < nrow;++i)
-    {
-        for(int j = csrptr[i];j < csrptr[i];++j)
-        {
-            for(int k = 0;k < kdim;k++)
-            {
-                float val = values[j];
-                int col = colind[j];
-                out[i * kdim + k] += val * dense[col * kdim + k];
-            }
-        }
+void spmm(int nrow, int kdim, int *csrptr, int *colind, float *values,
+          float *dense, float *out) {
+  for (int i = 0; i < nrow; ++i) {
+    for (int j = csrptr[i]; j < csrptr[i + 1]; ++j) {
+      for (int k = 0; k < kdim; k++) {
+        float val = values[j];
+        int col = colind[j];
+        out[i * kdim + k] += val * dense[col * kdim + k];
+      }
     }
+  }
 }
 
-
-void COO_to_CSR(std::vector<int>& row_CSR, std::vector<int> row_COO,
-    int Size, int matrixRow)
-{
-    row_CSR.push_back(0);
-    if (row_COO[0] != 0)
-    {
-        for (int j = 0; j < row_COO[0]; j++)
-            row_CSR.push_back(0);
+void COO_to_CSR(std::vector<int> &row_CSR, std::vector<int> row_COO, int Size,
+                int matrixRow) {
+  row_CSR.push_back(0);
+  if (row_COO[0] != 0) {
+    for (int j = 0; j < row_COO[0]; j++)
+      row_CSR.push_back(0);
+  }
+  for (int i = 0; i < (Size - 1); i++) {
+    for (int j = 0; j < row_COO[i + 1] - row_COO[i]; j++) {
+      row_CSR.push_back(i + 1);
     }
-    for (int i = 0; i < (Size - 1); i++)
-    {
-        for (int j = 0; j < row_COO[i + 1] - row_COO[i]; j++)
-        {
-            row_CSR.push_back(i + 1);
-        }
-    }
-    for (int j = 0; j < matrixRow + 1 - row_COO.back(); j++)
-    {
-        row_CSR.push_back(static_cast<int>(Size));
-    }
+  }
+  for (int j = 0; j < matrixRow + 1 - row_COO.back(); j++) {
+    row_CSR.push_back(static_cast<int>(Size));
+  }
 }
 
-#endif //SPMM_H
+#endif // SPMM_H
