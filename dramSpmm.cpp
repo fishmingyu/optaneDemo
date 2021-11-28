7095e38343c63d51f962bb7dabb7650a2328afeb
fishming <1661342068@qq.com>
2021-11-29 00:24:45 +0800

    omp spmm

--- dramSpmm.cpp
+++ dramSpmm.cpp
@@ -6,16 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#define ITER 200
 
 // dense [nrow, kdim]
 // out [nrow, kdim]
 
-template <class T> class DramArray {
+template <class T> class alignas(32) DramArray {
 public:
   DramArray(int num) {
     size = num * sizeof(T);
     array = (T *)malloc(size);
   }
+  void reset() { memset(array, 0, sizeof(T) * size); }
   ~DramArray() { free(array); }
   size_t size;
   T *array;
@@ -25,7 +27,7 @@ int main(int argc, char **argv) {
   int nrow;
   int ncol;
   int nnz;
-  int kdim = 4;
+  int kdim = atoi(argv[2]);
   std::vector<int> row_CSR, row_COO, col_COO;
   std::vector<float> values_COO;
   readMtx<float>(argv[1], row_COO, col_COO, values_COO, nrow, ncol, nnz);
@@ -35,7 +37,8 @@ int main(int argc, char **argv) {
   DramArray<int> colind(nnz);
   DramArray<float> values(nnz);
   DramArray<float> dense(nrow * kdim);
-  DramArray<float> out(nrow * kdim);
+  DramArray<float> out1(nrow * kdim);
+  DramArray<float> out2(nrow * kdim);
 
   std::copy(row_CSR.begin(), row_CSR.end(), csrptr.array);
   std::copy(col_COO.begin(), col_COO.end(), colind.array);
@@ -43,12 +46,30 @@ int main(int argc, char **argv) {
 
   srand(0);
   for (int i = 0; i < nrow * kdim; i++) {
-    dense.array[i] = ((rand() % INT_MAX) / INT_MAX * 1.0);
+    dense.array[i] = ((rand() % INT_MAX) * 1.0 / INT_MAX);
   }
-  unsigned long start = now();
+  // warp up
+  spmm_omp32(nrow, kdim, csrptr.array, colind.array, values.array, dense.array,
+             out1.array);
   spmm(nrow, kdim, csrptr.array, colind.array, values.array, dense.array,
-       out.array);
+       out2.array);
+
+  for (int i = 0; i < 100; i++)
+    if (out1.array[i] - out2.array[i] > 1e-5) {
+      printf("mistake %d %f, %f\n", i, out1.array[i], out2.array[i]);
+      // break;
+    }
+  unsigned long start = now();
+  for (int i = 0; i < ITER; i++)
+    spmm_omp32(nrow, kdim, csrptr.array, colind.array, values.array,
+               dense.array, out1.array);
   unsigned long end = now();
-  printf("finished %ld\n us", end - start);
+  printf("omp spmm finished %f s \n", (end - start) / 1e6 / ITER);
+  start = now();
+  for (int i = 0; i < ITER; i++)
+    spmm(nrow, kdim, csrptr.array, colind.array, values.array, dense.array,
+         out2.array);
+  end = now();
+  printf("serial spmm finished %f s \n", (end - start) / 1e6 / ITER);
   return 0;
 }
--- spmm.h
+++ spmm.h
@@ -1,6 +1,8 @@
 #ifndef SPMM_H
 #define SPMM_H
 
+#include "omp.h"
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -15,12 +17,156 @@ unsigned long now() {
 void spmm(int nrow, int kdim, int *csrptr, int *colind, float *values,
           float *dense, float *out) {
   for (int i = 0; i < nrow; ++i) {
-    for (int j = csrptr[i]; j < csrptr[i + 1]; ++j) {
+    int lb = csrptr[i];
+    int hb = csrptr[i + 1];
+    for (int j = lb; j < hb; ++j) {
+      float val = values[j];
+      int col = colind[j];
       for (int k = 0; k < kdim; k++) {
-        float val = values[j];
-        int col = colind[j];
         out[i * kdim + k] += val * dense[col * kdim + k];
       }
+    }
+  }
+}
+
+void spmm_omp(int nrow, int kdim, int *csrptr, int *colind, float *values,
+              float *dense, float *out) {
+  int i, j, k;
+#pragma omp parallel for num_threads(24) private(i, k, j)
+  for (i = 0; i < nrow; ++i) {
+    int lb = csrptr[i];
+    int hb = csrptr[i + 1];
+    int ktmp = kdim - (kdim % 8);
+#pragma unroll 4
+    // #pragma omp parallel for
+    for (k = 0; k < (kdim / 8); k++) {
+      __m256 valc = _mm256_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        __m256 valb = _mm256_loadu_ps(dense + col * kdim + k * 8);
+        float val = values[j];
+        __m256 vala = _mm256_broadcast_ss(&val);
+        valc = _mm256_fmadd_ps(vala, valb, valc);
+      }
+      _mm256_storeu_ps(out + i * kdim + k * 8, valc);
+    }
+#pragma unroll 4
+    for (int k = ktmp; k < kdim; k++) {
+      float acc = 0;
+      for (int j = lb; j < hb; ++j) {
+        float val = values[j];
+        int col = colind[j];
+        acc += val * dense[col * kdim + k];
+      }
+      out[i * kdim + k] = acc;
+    }
+  }
+}
+
+void spmm_omp16(int nrow, int kdim, int *csrptr, int *colind, float *values,
+                float *dense, float *out) {
+  int i, j, k;
+#pragma omp parallel for num_threads(24) private(i, k, j)
+  for (i = 0; i < nrow; ++i) {
+    int lb = csrptr[i];
+    int hb = csrptr[i + 1];
+    int ktmp16 = kdim - (kdim % 16);
+    int ktmp8 = kdim - (kdim % 8);
+#pragma unroll 4
+    // #pragma omp parallel for
+    for (k = 0; k < (kdim / 16); k++) {
+      __m512 valc = _mm512_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        float val = values[j];
+        __m512 valb = _mm512_loadu_ps(dense + col * kdim + k * 16);
+        __m512 vala = _mm512_set1_ps(val);
+        valc = _mm512_fmadd_ps(vala, valb, valc);
+      }
+      _mm512_storeu_ps(out + i * kdim + k * 16, valc);
+    }
+    for (int k = ktmp16; k < ktmp8; k++) {
+      __m256 valc = _mm256_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        __m256 valb = _mm256_loadu_ps(dense + col * kdim + ktmp16);
+        float val = values[j];
+        __m256 vala = _mm256_broadcast_ss(&val);
+        valc = _mm256_fmadd_ps(vala, valb, valc);
+      }
+      _mm256_storeu_ps(out + i * kdim + ktmp16, valc);
+    }
+#pragma unroll 4
+    for (int k = ktmp8; k < kdim; k++) {
+      float acc = 0;
+      for (int j = lb; j < hb; ++j) {
+        float val = values[j];
+        int col = colind[j];
+        acc += val * dense[col * kdim + k];
+      }
+      out[i * kdim + k] = acc;
+    }
+  }
+}
+
+void spmm_omp32(int nrow, int kdim, int *csrptr, int *colind, float *values,
+                float *dense, float *out) {
+  int i, j, k;
+#pragma omp parallel for num_threads(24) private(i, k, j)
+  for (i = 0; i < nrow; ++i) {
+    int lb = csrptr[i];
+    int hb = csrptr[i + 1];
+    int ktmp32 = kdim - (kdim % 32);
+    int ktmp16 = kdim - (kdim % 16);
+    int ktmp8 = kdim - (kdim % 8);
+#pragma unroll 4
+    // #pragma omp parallel for
+    for (k = 0; k < (kdim / 32); k++) {
+      __m512 valc1 = _mm512_setzero_ps();
+      __m512 valc2 = _mm512_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        float val = values[j];
+        __m512 valb1 = _mm512_loadu_ps(dense + col * kdim + k * 32);
+        __m512 valb2 = _mm512_loadu_ps(dense + col * kdim + k * 32 + 16);
+        __m512 vala = _mm512_set1_ps(val);
+        valc1 = _mm512_fmadd_ps(vala, valb1, valc1);
+        valc2 = _mm512_fmadd_ps(vala, valb2, valc2);
+      }
+      _mm512_storeu_ps(out + i * kdim + k * 32, valc1);
+      _mm512_storeu_ps(out + i * kdim + k * 32 + 16, valc2);
+    }
+    for (k = ktmp32; k < ktmp16; k += 16) {
+      __m512 valc = _mm512_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        float val = values[j];
+        __m512 valb = _mm512_loadu_ps(dense + col * kdim + ktmp32 + k);
+        __m512 vala = _mm512_set1_ps(val);
+        valc = _mm512_fmadd_ps(vala, valb, valc);
+      }
+      _mm512_storeu_ps(out + i * kdim + ktmp32 + k, valc);
+    }
+    for (int k = ktmp16; k < ktmp8; k += 8) {
+      __m256 valc = _mm256_setzero_ps();
+      for (j = lb; j < hb; ++j) {
+        int col = colind[j];
+        float val = values[j];
+        __m256 valb = _mm256_loadu_ps(dense + col * kdim + ktmp16 + k);
+        __m256 vala = _mm256_broadcast_ss(&val);
+        valc = _mm256_fmadd_ps(vala, valb, valc);
+      }
+      _mm256_storeu_ps(out + i * kdim + ktmp16 + k, valc);
+    }
+#pragma unroll 4
+    for (int k = ktmp8; k < kdim; k++) {
+      float acc = 0;
+      for (int j = lb; j < hb; ++j) {
+        float val = values[j];
+        int col = colind[j];
+        acc += val * dense[col * kdim + k];
+      }
+      out[i * kdim + k] = acc;
     }
   }
 }
